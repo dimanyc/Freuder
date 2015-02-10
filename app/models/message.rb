23029a72d3b6c0960d5b1d9b6d4305fb9a5a7a16
dimanyc <dimitrynazarov@gmail.com>
2015-02-10 13:37:37 -0500

    finished Message model validation testing

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,12 @@
 class Message < ActiveRecord::Base
 
-# Set columns as Array: 
-serialize :slipped, Array
-serialize :hashtags, Array
-serialize :mentions, Array
+  # Set columns to Array data type: 
+  serialize :slipped, Array
+  serialize :hashtags, Array
+  serialize :mentions, Array
 
+  # Validations:
+  validates :body, presence: true, uniqueness: true, length: { within: 3...143, too_long: "body is too long", too_short: "body is too short" }
+  validates :author, presence: true, uniqueness: true
 
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,52 +2,62 @@ require 'rails_helper'
 
 RSpec.describe Message, :type => :model do
   
-  context 'standard Tweet' do 
-  
-  before(:each) do 
-    @message = build(:message)
-  end
-
-  ### Columns to Array initiation
-    it 'has Array data types in Slipped, Hashtags and Mentions' do 
-      expect(@message.slipped).to eq([])
-      expect(@message.hashtags).to eq([])
-      expect(@message.mentions).to eq([])
-    end
-
   ### Factory
-    it 'has a valid factory' do
-      expect(@message).to be_valid
-    end
-
-    it 'accepts array appending to Slipped, Hashtags and Mentions' do 
-      expect(@message.slipped << "foobarrito").to eq(["foobarrito"])
-    end
+  it 'has a valid factory' do
+    expect(build(:message)).to be_valid
   end
+
   ### Validations:
 
   #### Presence:
   it 'is not valid without a body' do
-    pending
+    message = build(:message, body: nil)
+    message.valid?
+    expect(message.errors[:body]).to include ("can't be blank")
   end
 
   it 'is not valid without an author' do 
-    pending
-  end
-
-
-  #### Body length:
-  it 'is not valid with body length of < 3 characters' do 
-    pending
-  end
-
-  it 'is not valid with body length of > 140 characters' do 
-    pending
+    message = build(:message, author: nil)
+    expect(message).to_not be_valid
   end
 
   #### Uniquness: 
   it 'is not valid with a duplicate body' do
-    pending
+    create(:message, body: "foobarrito")
+    duplicate_message = build(:message, body: "foobarrito")
+    duplicate_message.valid?
+    expect(duplicate_message.errors[:body]).to include ("has already been taken")
+  end
+
+  #### Columns to Array initiation
+  it 'has Array data types in Slipped, Hashtags and Mentions columns' do 
+    message = build(:message)
+    expect(message.slipped).to eq([])
+    expect(message.hashtags).to eq([])
+    expect(message.mentions).to eq([])
+  end
+
+  ### Tweet: 
+  context 'Tweet' do 
+  
+    before(:example) do 
+      @message = build(:message)
+    end
+
+    it 'accepts array data appending in Slipped, Hashtags and Mentions columns / objects' do 
+      expect(@message.slipped << "foobarrito").to eq(["foobarrito"])
+    end
+
+    #### Body length:
+    it 'is not valid with body length of < 3 or > 143 characters' do
+      message1 = build(:message, body: "fo")
+      message2 = build(:message, body: "#{@message.body}" + "xyz") 
+      message1.valid?
+      expect(message1.errors[:body]).to include ("body is too short")
+      message2.valid?
+      expect(message2.errors[:body]).to include ("body is too long")
+    end
+
   end
 
 end
