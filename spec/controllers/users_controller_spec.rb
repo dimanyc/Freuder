b7d40717a1f031fa909555f19dcf1af991ce9021
dimanyc <dimitrynazarov@gmail.com>
2015-02-18 15:52:05 -0500

    adding @message controller spec for users#show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,19 +4,59 @@ RSpec.describe UsersController, :type => :controller do
 
   describe 'GET #show' do
 
-    before(:each) do 
-      @user = create(:user)
-      get :show, id: @user.id
+    context 'when authenticated' do 
+
+      # add authenticate_
+
+      before(:each) do 
+        @user = create(:user)
+        get :show, id: @user.id
+      end
+
+      it "assigns the requested user to @user" do
+        expect(assigns(:user)).to eq @user
+      end
+
+      it "renders the :show template" do
+        expect(response).to render_template :show
+      end
+
+      it "loads user profile image" do
+        expect(@user.image_url).to_not eq nil 
+      end
+
+      context "loads User's messages"
+
+        before(:each) do 
+          @messages = @user.messages
+          @new_user_message = create(:message, owner: @user)
+        end
+
+        it 'and creates @messages instance variable' do 
+          expect(@messages).to_not eq nil
+        end
+
+        it "and puts each new message in @messages" do 
+
+            expect(@messages).to include @new_user_message
+            puts @new_user_message.owner_id  
+
+        end
+
+        it "creates new @message" do 
+
+        end
+
+      
+
     end
 
-    it "assigns the requested user to @user" do
-      expect(assigns(:user)).to eq @user
-    end
+    context 'when not authenticated' do 
 
 
-    it "renders the :show template" do
-      expect(response).to render_template :show
+
     end
+
 
   end
 
