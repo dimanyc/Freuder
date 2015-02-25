2407a860b15b893b89dc11f55e57704e44f5bf64
dimanyc <dimitrynazarov@gmail.com>
2015-02-25 17:00:28 -0500

    seriazlied if changed added

--- app/controllers/filters_controller.rb
+++ app/controllers/filters_controller.rb
@@ -1,8 +1,6 @@
 class FiltersController < ApplicationController
-  before_action :set_filter, only: [:show, :edit, :update, :destroy]
-  
-  ### Create
 
+  ### Create
   def new
     @filter = current_user.filters.new
   end
@@ -10,55 +8,20 @@ class FiltersController < ApplicationController
   def create
     @filter = current_user.filters.new(filter_params)
 
-    if @filter.save
+    if @filter.save!
       redirect_to user_path(current_user)
       flash[:notice] = "Filter #{@filter.name} has been created"
     else
-      render :new
+      redirect_to user_path(current_user)
       flash[:alert] = "Problem creating this filter"
     end
 
-    
-
   end
-
-
-
-
-
-  # def create
-  #   @filter = current_user.filters.create(filter_params)
-  #   respond_to do |format|
-  #   if @filter.save
-        
-  #         format.html { redirect_to user_path(current_user), notice: "Filter #{filter.name} has been created!" }
-  #         format.json { render json: @filter, status: :created, location: current_user.filters }
-
-  #     else 
-        
-  #         format.html { render :new, alert: "Problem adding your filter. Try again, or contact me: @dimanyc" }
-  #         format.json { render json: @filter.errors, status: :unprocessable_entity }
-        
-  #     end
-  #   end
-  # end
-  
-  ### Read: 
-  def index
-    #@filters = current_user.filters
-  end 
-
-  def show
-  end 
 
   private
-
-  def set_filter
-    self.slips = []
-    
-  end
 
   def filter_params
     params.require(:kozlina).permit(:name,:description,:slips,:user_id,:case_sensitive,:keep_slips_order)
   end
+
 end
--- app/models/filter.rb
+++ app/models/filter.rb
@@ -3,21 +3,12 @@ class Filter < ActiveRecord::Base
   ### Active Relations:
   belongs_to :user
   has_many :messages, as: :processor
+ 
+  ### Serialize to array:
+  serialize :slips, Array if changed?
   
-  ### Set columns to Array data type: 
-  after_save :slips_to_array
-
 
   ### Validations:
   validates :name, :slips, presence: true
-
-  # def slips= stuff
-  #   serialized_slips = stuff.split(', ')
-  # end
-
-  def slips_to_array
-    slips.split(', ')
-    self.save
-  end
 
 end
