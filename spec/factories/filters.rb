028ba5b41af6b106f18a6f90ea61027cc7d0895a
dimanyc <dimitrynazarov@gmail.com>
2015-02-27 19:02:47 -0500

    adding invalid_filter factories

--- spec/factories/filters.rb
+++ spec/factories/filters.rb
@@ -8,23 +8,13 @@ FactoryGirl.define do
     keep_slips_order false
   end
 
-  factory :invalid_filter, parent: :filter do
+  factory :filter_with_empty_name, parent: :filter do
     name nil
-    slips nil
   end
 
-  # factory :user do 
-  #   sequence(:username) { |sequenced_number| "user#{sequenced_number}" }
-  #   sequence(:email) { |sequenced_number| "foo#{sequenced_number}@bar.com" }
-  #   sequence(:uid) { |sequenced_number| "12312#{sequenced_number}" }
-  #   provider 'twitter'
-  #   sequence(:image_url) { |sequenced_number| "https://pbs.twimg.com/profile_images/558793827993911297/Fo7GFEY#{sequenced_number}_bigger.jpeg" }
-  # end
-
-  # factory :invalid_user, parent: :user do
-  #   username nil
-  #   uid nil
-  # end
+  factory :filter_with_empty_slips, parent: :filter do
+    slips nil
+  end
 
 end
 
