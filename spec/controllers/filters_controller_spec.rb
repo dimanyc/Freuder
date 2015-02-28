0129826489c1a73c808124a2dfdc6da4ae6cebcd
dimanyc <dimitrynazarov@gmail.com>
2015-02-28 13:43:08 -0500

    fixed up the scss

--- spec/controllers/filters_controller_spec.rb
+++ spec/controllers/filters_controller_spec.rb
@@ -23,6 +23,41 @@ RSpec.describe FiltersController, :type => :controller do
 
       end
 
+      context 'without valid attributes' do 
+
+        it "doesn't save a Filter with empty 'name' to db " do
+          expect {
+            post :create, user_id: 1, filter: attributes_for(:filter_with_empty_name)
+          }.to_not change(Filter, :count)
+        end
+
+        it "doesn't save a Filter with empty 'slips' to db " do
+          expect {
+            post :create, user_id: 1, filter: attributes_for(:filter_with_empty_slips)
+          }.to_not change(Filter, :count)
+        end
+
+        it "re-renders the user#show view" do
+          post :create, user_id: 1, filter: attributes_for(:filter_with_empty_name)
+          expect(response).to render_template :new
+        end
+
+      end
+
 	end
+
+  describe "DELETE #destroy" do
+    
+    before (:each) do 
+      @filter = create(:filter)
+    end
+
+    it 'deletes the filter' do 
+      expect {
+        delete :destroy, user_id: 1, filter: attributes_for(:filter)
+      }.to change(Filter, :count).by(-1)
+    end
+    
+  end
 
 end
